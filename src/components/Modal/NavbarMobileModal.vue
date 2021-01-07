55b78565c4686bafc183c94304b7d1067b29758c
joseph <jpire.dev@gmail.com>
2021-01-07 11:05:36 +0100

    refactor(template): new navigation components

--- src/components/Modal/NavbarMobileModal.vue
+++ src/components/Modal/NavbarMobileModal.vue
@@ -5,60 +5,17 @@
       <div class="menu-links">
         <ul>
           <li
-            v-for="navItem in $static.metadata.headerNavigation"
-            :key="navItem.name"
-            class="py-1"
+            v-for="section in $static.allSection.edges"
+            :key="section.node.title"
+            class="py-1 flex justify-start"
           >
             <g-link
               class="block py-1"
-              :to="navItem.link"
-              :title="navItem.name"
-              v-if="navItem.external!=true && navItem.children.length <=0"
-            >{{ navItem.name}}</g-link>
-            <a
-              class="block"
-              :href="navItem.link"
-              target="_blank"
-              :title="navItem.name"
-              v-if="navItem.external==true && navItem.children.length <=0"
-            >{{ navItem.name}}</a>
-            <ClientOnly>
-              <v-popover
-                placement="right"
-                popoverClass="mobile-navbar-popover"
-                offset="16"
-                v-if="navItem.children.length > 0"
-              >
-                <a class="block py-1" style="cursor:pointer;">
-                  {{ navItem.name }}
-                  <font-awesome :icon="['fas', 'angle-right']"></font-awesome>
-                </a>
-
-                <template slot="popover">
-                  <ul>
-                    <li
-                      v-for="subItem in navItem.children"
-                      :key="subItem.name"
-                      class="px-4 py-2 submenu-item hover:text-white"
-                    >
-                      <g-link
-                        class="block"
-                        :to="subItem.link"
-                        :title="subItem.name"
-                        v-if="subItem.external!=true"
-                      >{{ subItem.name}}</g-link>
-                      <a
-                        class="block"
-                        :href="subItem.link"
-                        target="_blank"
-                        :title="subItem.name"
-                        v-if="subItem.external==true"
-                      >{{ subItem.name}}</a>
-                    </li>
-                  </ul>
-                </template>
-              </v-popover>
-            </ClientOnly>
+              :to="section.node.path"
+              :title="section.node.name"
+            >
+              {{ section.node.title }}
+            </g-link>
           </li>
         </ul>
       </div>
@@ -69,19 +26,17 @@
   </div>
 </template>
 
-
 <static-query>
 query {
    metadata {
     siteName
-    headerNavigation {
-      name
-      link
-      external
-      children {
-        name
-        link
-        external
+  },
+  allSection {
+    edges{
+      node{
+        title, 
+        description, 
+        path
       }
     }
   }
@@ -92,10 +47,9 @@ import Subnavigation from "~/components/Navbar/NavbarSubNavigation.vue";
 
 export default {
   components: {
-    Subnavigation
-  }
+    Subnavigation,
+  },
 };
 </script>
 
-<style>
-</style>
+<style></style>
